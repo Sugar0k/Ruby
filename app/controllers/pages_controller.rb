a22d2a07fb8006b355d44488622b5fb6928f9b96
Stesniashka <wifi.router.52@gmail.com>
2021-06-16 00:51:11 +0300

    Init

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def about
+    @heading = 'Dev by Me!'
+    @body = 'Pavlyuk Alexsandr Petrovich\nIVT-18-2'
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  has_many :marks
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 10 }
+end
